e108ed489995a4e0d5ab8fbfc004c0214c168b97
kakrotto <jiezhao458@gmail.com>
2022-03-17 20:09:01 +0800

    update

--- Leetcode/Easy/509.py
+++ Leetcode/Easy/509.py
@@ -2,15 +2,25 @@
 # -*- coding: UTF-8 -*-
 # @Time   :  2022/3/16 6:09 下午
 # @Author :  Allen
+# 斐波那契数
 
-class Solution:
+class Solution1:
     def fib(self, n: int) -> int:
         if n <= 1:
-            return 1
+            return n
         return self.fib(n-1) + self.fib(n-2)
 
 
-s = Solution()
-print(s.fib(3))
+class Solution2:
+    def fib(self, n: int) -> int:
+        if n < 2:
+            return n
+        a, b, c = 0, 1, 0
+        for i in range(1, n):
+            c = a + b
+            a, b = b, c
+        return c
 
-"0 + 1 + 1 + 2 + 3"
+
+if __name__ == '__main__':
+    pass
